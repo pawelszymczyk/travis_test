12c5b9bc7097ed84a874c16042f6e53f7c9a29dc
Pawel Szymczyk <pawel.szymczyk@allegro.pl>
2015-06-18 16:15:27 +0200

    ble ble

--- src/test/java/pl/pawel/TestTest.java
+++ src/test/java/pl/pawel/TestTest.java
@@ -9,15 +9,11 @@ import java.net.Socket;
 public class TestTest {
 
     @Test
-    public void test1() {
+    public void test1() throws IOException {
         Main.main();
 
-        try {
             Socket s = new Socket("localhost", 8080);
             s.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @AfterClass
